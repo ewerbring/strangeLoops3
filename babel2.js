e8eeacd38729d4ffc65c48ae5ec95a79b06c1e00
Carl-Gustaf <ewerbring@gmail.com>
2019-05-14 13:00:23 -0400

    first

---
+++ babel2.js
@@ -0,0 +1,92 @@
+$('.hbox1').click(function() {
+   $('.text1').show();
+   $('.hbox1').hide();
+
+});
+$('.text1').click(function() {
+   $('.text2').show();
+});
+$('.text2').click(function() {
+   $('.text3').show();
+});
+$('.text3').click(function() {
+   $('.text4').show();
+});
+$('.text4').click(function() {
+   $('.text5').show();
+});
+
+$('#p2').click(function() {
+   $('#next').show();
+});
+
+$('#showill').click(function() {
+   $('.ill').show();
+});
+
+///stonecutter
+
+$('#sc1').click(function() {
+   $('#sc1').hide();
+   $('#sc2').show();
+});
+$('#sc2').click(function() {
+   $('#sc2').hide();
+   $('#sc3').show();
+});
+$('#sc3').click(function() {
+   $('#sc3').hide();
+   $('#sc4').show();
+});
+$('#sc4').click(function() {
+   $('#sc4').hide();
+   $('#sc5').show();
+});
+$('#sc5').click(function() {
+   $('#sc5').hide();
+   $('#sc6').show();
+});
+$('#sc6').click(function() {
+   $('#sc6').hide();
+   $('#sc7').show();
+});
+$('#sc7').click(function() {
+   $('#sc7').hide();
+   $('#sc8').show();
+});
+$('#sc8').click(function() {
+   $('#sc8').hide();
+   $('#sc9').show();
+});
+$('#sc9').click(function() {
+   $('#sc9').hide();
+   $('#sc10').show();
+});
+$('#sc10').click(function() {
+   $('#sc10').hide();
+   $('#sc11').show();
+});
+$('#sc11').click(function() {
+   $('#sc11').hide();
+   $('#sc12').show();
+});
+$('#sc12').click(function() {
+   $('#sc12').hide();
+   $('#sc13').show();
+});
+$('#sc13').click(function() {
+   $('#sc13').hide();
+   $('#sc14').show();
+});
+$('#sc14').click(function() {
+   $('#sc14').hide();
+   $('#sc15').show();
+});
+
+
+
+/////rockpapersissor
+$('.rock').click(function() {
+   $('.rock').hide();
+   $('.rock2').show();
+});
